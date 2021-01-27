16067ab7b4033468d318c73d5af5c31a44b9c66d
Andreas Olsson <andreas.o.olsson@outlook.com>
2021-01-27 10:39:23 +0100

    Defined routes

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+// Connect to database
+connectDB();
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 app.get('/', (req, res) => res.send('API Running'));
 
