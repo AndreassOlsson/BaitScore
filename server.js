440b1e20897297e89f2ed9343b28b0d794967b60
Andreas Olsson <andreas.o.olsson@outlook.com>
2021-01-27 11:12:48 +0100

    Request testing

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define routes
+app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-
-app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
