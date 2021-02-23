94cb4356a5812698fcddba858c98d53a8e6bd76c
Andreas Olsson <andreas.o.olsson@outlook.com>
2021-02-23 17:09:32 +0100

    Added register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -12,40 +12,73 @@ const Register = () => {
 
   const { name, email, password, password_confirm } = formData;
 
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password_confirm) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className='large textbox'>Sign Up</h1>
       <p className='lead'>
         <i className='fas fa-user'></i>Create your account
       </p>
-      <form className='form' action='create-profile.html'></form>
-      <div className='form-group'>
-        <input type='text' placeholder='Name' name='name' required />
-      </div>
-      <div className='form-group'>
-        <input type='email' placeholder='Email Adress' name='email' />
-        <small className='form-text'>
-          This site uses gravatar, so if you want a profile image, use a
-          Gravatar email
-        </small>
-      </div>
-      <div className='form-group'>
-        <input
-          type='password'
-          placeholder='Password'
-          name='password'
-          minLength='6'
-        />
-      </div>
-      <div className='form-group'>
-        <input
-          type='password'
-          placeholder='Confirm Password'
-          name='password_confirm'
-          minLength='6'
-        />
-      </div>
-      <input type='submit' className='btn btn-primary' value='Register' />
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Adress'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password_confirm'
+            value={password_confirm}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
       <p className='my-1'>
         Already have an account? <Link to='/login'>Sign in</Link>
       </p>
